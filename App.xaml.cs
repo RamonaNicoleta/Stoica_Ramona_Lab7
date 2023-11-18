898fef27146b3645db2e309c286c548c278966fb
RamonaNicoleta <stcrmn@gmail.com>
2023-11-18 13:25:50 +0200

    Laboratorul 8 facut complet

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace Stoica_Ramona_Lab7
+﻿using System;
+using Stoica_Ramona_Lab7.Data;
+using System.IO;
+
+
+namespace Stoica_Ramona_Lab7
 {
     public partial class App : Application
     {
+
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
