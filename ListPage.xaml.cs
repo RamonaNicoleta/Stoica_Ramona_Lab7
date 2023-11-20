f297acd3bf458b1ad72f970853610eae69fd725f
RamonaNicoleta <stcrmn@gmail.com>
2023-11-20 19:03:23 +0200

    laboratorul 9 complet

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -1,12 +1,22 @@
+
 using Stoica_Ramona_Lab7.Models;
+using System.Xml.Linq;
+
 namespace Stoica_Ramona_Lab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
-	{
-		InitializeComponent();
-	}
+    public ListPage()
+    {
+        InitializeComponent();
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
 
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
@@ -17,9 +27,35 @@ public partial class ListPage : ContentPage
     }
     async void OnDeleteButtonClicked(object sender, EventArgs e)
     {
-        var slist = (ShopList)BindingContext;
-        await App.Database.DeleteShopListAsync(slist);
-        await Navigation.PopAsync();
+        if (listView.SelectedItem != null)
+        {
+            // Get the selected product from the ListView
+            Product selectedProduct = listView.SelectedItem as Product;
+
+            // Delete the selected product from the database
+            var result = await App.Database.DeleteProductAsync(selectedProduct);
+
+            if (result > 0)
+            {
+                // Refresh the ListView after deletion
+                var shopList = (ShopList)BindingContext;
+                listView.ItemsSource = await App.Database.GetListProductsAsync(shopList.ID);
+            }
+            else
+            {
+                // Handle the case where deletion was unsuccessful
+                // You can display a message or perform any necessary actions
+            }
+        }
+    }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+ 
     }
 
-}
---
+++ Models/ListProduct.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+using SQLiteNetExtensions.Attributes;
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stoica_Ramona_Lab7.Models
+{
+     public class ListProduct
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        [ForeignKey(typeof(ShopList))]
+        public int ShopListID { get; set; }
+        public int ProductID { get; set; }
+
+    }
+}
